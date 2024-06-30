efde8fc97246bd727bebe8887b9282e4107104b7
Amol Lahane <amoldinlahane@gmail.com>
2024-06-30 14:39:12 +0530

    two

---
+++ 01vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Youtube from "./Youtube";
+
+
+function App() {
+  const username = "hitesh"; 
+  return (
+    <>
+      <h1>vite react app: {""}</h1> 
+      <h1>vite react app: {username}</h1>
+      <Youtube />
+    </>
+  );
+}
+
+export default App;
